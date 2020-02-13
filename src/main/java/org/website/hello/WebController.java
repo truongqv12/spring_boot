88d35fe2da120b1b574565551b70bfbabd2a2afd
TTTruong <tttruong@vnpt.vn>
2020-02-13 16:45:20 +0700

    first commit

---
+++ src/main/java/org/website/hello/WebController.java
@@ -0,0 +1,30 @@
+package org.website.hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@PropertySource("/applications.properties")
+
+public class WebController {
+    // Đón nhận request GET
+    @GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
+    public String index() {
+        return "index"; // Trả về file index.html
+    }
+
+    @GetMapping("/hello")
+    public String hello(
+            // Request param "name" sẽ được gán giá trị vào biến String
+            @RequestParam(name = "name", required = false, defaultValue = "") String name,
+            // Model là một object của Spring Boot, được gắn vào trong mọi request.
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
