e5aea145e8088b785aa1e435e4471bda347a8b2e
Trần Trọng Trường <truongqv13@gmail.com>
2020-02-16 21:50:14 +0700

    request

---
+++ src/main/java/org/website/hello/Todo.java
@@ -0,0 +1,12 @@
+package org.website.hello;
+
+import lombok.Data;
+
+/**
+ * TẠo ra class này để hiển thị thông tin
+ */
+@Data
+public class Todo {
+    public String title;
+    public String detail;
+}
--- src/main/java/org/website/hello/WebController.java
+++ src/main/java/org/website/hello/WebController.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Controller
 @PropertySource("/applications.properties")
@@ -45,5 +48,36 @@ public class WebController {
 
         // TRả về template profile.html
         return "profile";
+    }
+
+    // Sử dụng tạm List để chứa danh sách công việc
+    // Thay cho Database.
+    // Chỉ dùng cách này khi DEMO ^^
+    List<Todo> todoList = new CopyOnWriteArrayList<>();
+
+    @GetMapping("/listTodo")
+    public String indexList(
+            Model model,
+            @RequestParam(value = "limit", required = false) Integer limit
+    ) {
+        // Trả về đối tượng todoList.
+        // Nếu người dùng gửi lên param limit thì trả về sublist của todoList
+        model.addAttribute("todoList", limit != null ? todoList.subList(0, limit) : todoList);
+
+        // Trả về template "listTodo.html"
+        return "listTodo";
+    }
+
+    @GetMapping("/addTodo")
+    public String addTodo(Model model) {
+        // Thêm mới một đối tượng Todo vào model
+        model.addAttribute("todo", new Todo());
+        // Trả về template addTodo.html
+        return "addTodo";
+    }
+    @PostMapping("/addTodo")
+    public String addTodo(@ModelAttribute Todo todo) {
+        todoList.add(todo);
+        return "success";
     }
 }
